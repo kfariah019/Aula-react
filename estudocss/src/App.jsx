613af08eabecc148a5267ddb57be06a72ba3c1a2
kfariah019 <kauanfaria.hf@gmail.com>
2025-07-29 12:26:53 -0300

    29/07

--- estudocss/src/App.jsx
+++ estudocss/src/App.jsx
@@ -1,9 +1,30 @@
+import"./App.css";
+
 export default function App()
 {
+    const estilo = {
+        color:"red",
+        textAling:
+    }
     return (
-        <div>
+        <div className ="conteudo">
             <h1>Estudo CSS</h1>
-            
+            <p> o CSS (<i>Casting Style Sheets</i> )é uma linguagem ultilizada para estilizar as paginas Web com ela podemos definir:</p>
+            <ul>
+                <li> Cores de fundo, do texto e dos elementos.</li>
+                <li>fontes e tamanhos das letras</li>
+                <li>Posicionamento  dos elementos da pagina</li>
+                <li>espacamentos, bordas, sombras, etc.</li>
+                <li>Efeitos visuais e animações</li>
+
+
+
+
+            </ul>
+            <h3 style= {}
+            <p>
+<img src="fortagua.jpg"alt=""/>
+            </p>
         </div>
     );
 }
